0319294bae0b8e18b13644bc53cc575dab2b9684
liyakat ali <aliliyakat902@gmail.com>
2025-02-22 11:26:43 +0800

    Added logging information.

--- backend/db_helper.py
+++ backend/db_helper.py
@@ -1,8 +1,11 @@
-# ------- CRUD -------
+# ------- CRUD Operations-------
 
 import mysql.connector
-
 from contextlib import contextmanager
+from logging_setup import setup_logger
+
+
+logger = setup_logger('db_helper')
 
 @contextmanager
 def get_db_cursor(commit=False):
@@ -24,6 +27,7 @@ def get_db_cursor(commit=False):
 
 
 def fetch_expense_for_date(expense_date):
+    logger.info(f"fetch_expense_for_date called with {expense_date}")
     with get_db_cursor() as cursor:
         cursor.execute("SELECT * FROM expenses WHERE expense_date = %s", (expense_date, ))
         expenses = cursor.fetchall()
@@ -31,16 +35,19 @@ def fetch_expense_for_date(expense_date):
     
 
 def delete_expense_for_date(expense_date):
+    logger.info(f"delete_expense_for_date called with {expense_date}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute("DELETE FROM expenses WHERE expense_date = %s", (expense_date, ))
 
 
 def insert_expense(expense_date, amount, category, notes):
+    logger.info(f"insert_expense called with date: {expense_date}, amount: {amount}, category: {category}, notes: {notes}")
     with get_db_cursor(commit=True) as cursor:
         cursor.execute("INSERT INTO expenses (expense_date, amount, category, notes) VALUES (%s, %s, %s, %s)", (expense_date, amount, category, notes))
 
 
 def fetch_expense_summary(start_date, end_date):
+    logger.info(f"fetch_expense_summary called with start {start_date} and end {end_date}")
     with get_db_cursor()as cursor:
         cursor.execute("SELECT category, SUM(amount) as total FROM expenses WHERE expense_date BETWEEN %s AND %s GROUP By category",(start_date, end_date))
         data = cursor.fetchall()
@@ -49,6 +56,7 @@ def fetch_expense_summary(start_date, end_date):
 
 
 if __name__ == '__main__':
+    expenses = fetch_expense_for_date("2024-08-01")
     # insert_expense("2024-10-10", 200, "Food", "Eat Biryani")
     # delete_expense_for_date("2024-10-10")
     data = fetch_expense_summary("2024-08-01", "2024-08-05")
