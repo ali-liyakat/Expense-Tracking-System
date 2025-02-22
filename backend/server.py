338a7809b56a2170cb832f29c3973773e9401bbd
liyakat ali <aliliyakat902@gmail.com>
2025-02-22 22:59:41 +0800

    Added backend server code for Analytics tab.

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import date
 import db_helper
 from typing import List
 from pydantic import BaseModel
+
+app = FastAPI()
 
 
 class Expense(BaseModel):
@@ -10,11 +12,16 @@ class Expense(BaseModel):
     category: str
     notes: str
 
-app = FastAPI()
+
+class DateRange(BaseModel):
+    start_date: date
+    end_date: date
 
 @app.get("/expenses/{expense_date}", response_model=List[Expense])
 def get_expenses(expense_date: date):
     expenses = db_helper.fetch_expense_for_date(expense_date)
+    if expenses is None:
+        raise HTTPException(status_code = 500, detail="Failed to get expenses")
     return expenses
 
 
@@ -25,3 +32,25 @@ def add_or_update_expense(expense_date: date, expenses: List[Expense]):
     for expense in expenses:
         db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
     return {"message": "Expenses updated successfully"}
+
+
+
+@app.post("/analytics/")
+def get_analytics(date_range: DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+    if data is None:
+        raise HTTPException(status_code=500, detail="Failed to retrieve expense summary from the database.")
+
+    total = sum([row['total'] for row in data])
+
+    breakdown = {}
+    for row in data:
+        percentage = (row['total']/total)*100 if total != 0 else 0
+        breakdown[row['category']] = {
+            "total": row['total'],
+            "percentage": percentage
+        }
+
+    return breakdown
+
+    
